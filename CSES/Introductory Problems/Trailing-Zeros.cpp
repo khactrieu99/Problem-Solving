31057ebfba8b171b8a73303765f27dc0d599740e
khactrieu99 <khactrieu.personal@gmail.com>
2020-11-11 16:17:19 +0700

    update

--- CSES/Introductory Problems/Trailing-Zeros.cpp
+++ CSES/Introductory Problems/Trailing-Zeros.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int n;
+int res=0, temp;
 
 int main() {
     cin >> n;
+    // O(nlogn)
+    // for(int i=5; i<=n; i+=5) {
+    //     res++;
+    //     temp=i/5;
+    //     while(temp%5==0) {
+    //         res++;
+    //         temp/=5;
+    //     }
+    // }
+
+    // better way O(n)
+    while(n) res+=(n=n/5);
+    
+    cout << res << endl;
     return 0;
 }
