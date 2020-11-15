0b53768c7183ea23bd36c8ec2807c89481101761
khactrieu99 <khactrieu.personal@gmail.com>
2020-11-15 22:33:10 +0700

    update

--- CSES/Sorting and Searching/Movie-Festival.cpp
+++ CSES/Sorting and Searching/Movie-Festival.cpp
@@ -1,12 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, a, b;
+#define all(x) begin(x), end(x)
+int n, a, b, curr, ans=1;
+vector< pair<int,int> > v;
 
 int main() {
     cin >> n;
-    while(n--) {
+    for(int i=0; i<n; i++) {
         cin >> a >> b;
+        v.push_back({b,a});
     }
+    sort(all(v));
+
+    curr=v[0].first;
+    for(int i=1; i<n; i++) {
+        if(v[i].second>=curr) {
+            ans++;
+            curr=v[i].first;
+        }
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
