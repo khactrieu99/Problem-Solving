259eeba3ec68e221bfe3c927dfe522f31fa6b4f6
trieutrng <khactrieu.personal@gmail.com>
2020-11-24 17:24:26 +0700

    update

---
+++ CSES/Sorting and Searching/Subarray-Divisibility.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 200001
+
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+int n;
+vi a;
+
+int main() {
+	cin >> n;
+	a.resize(n);
+	
+	for(int i=0; i<n; i++) cin >> a[i];
+
+
+	return 0;
+}
