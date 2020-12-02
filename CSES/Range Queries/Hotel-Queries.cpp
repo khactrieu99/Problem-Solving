000585b797f0c3e1ed046a73f8a62d24f443e228
trieutrng <khactrieu.personal@gmail.com>
2020-12-02 14:38:05 +0700

    update

---
+++ CSES/Range Queries/Hotel-Queries.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 200001
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+
+int n, m, r;
+vi h;
+
+int main() {
+    cin >> n >> m;
+    h.resize(n);
+    for(auto &val: h) cin >> val;
+    
+    while(m--) {
+        cin >> r;
+    
+    }
+    return 0;
+}
