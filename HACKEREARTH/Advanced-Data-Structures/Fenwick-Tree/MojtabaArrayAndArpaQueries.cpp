2bed88f7d2943bfd28299b8b11a554b5b71aa273
trieutrng <khactrieu.personal@gmail.com>
2020-12-16 17:26:08 +0700

    update graph

---
+++ HACKEREARTH/Advanced-Data-Structures/Fenwick-Tree/MojtabaArrayAndArpaQueries.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 500001
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+
+int n, k, q;
+
+int main() {
+    cin >> n >> k >> q;
+    return 0;
+}
