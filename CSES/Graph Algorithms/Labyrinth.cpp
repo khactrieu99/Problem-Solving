2c41a56777301640e4bfe1527b9768b6df8ca2e0
khactrieu99 <khactrieu.personal@gmail.com>
2020-12-17 02:09:47 +0700

    update

--- CSES/Graph Algorithms/Labyrinth.cpp
+++ CSES/Graph Algorithms/Labyrinth.cpp
@@ -9,16 +9,39 @@ typedef struct {
     int x, y;
 } coor;
 
-int n, m, ;
-vc g, path;
-vb isVisited;
-coor start, end;
+typedef pair<coor,char> bfs; // first is coordinate (x,y), second is the move ['L','R','D','U']
 
-void findPath(int i, int j, char m) {
-    if(i==end.y&&j==end.x) return;
-    if(!isVisited[i][j]) {
-        isVisited[i][j]=true;
-        
+int x[] = {1, -1, 0, 0};
+int y[] = {0, 0, 1, -1};
+char m[] = {'R', 'L', 'D', 'U'};
+
+int n, m;
+bool flag=false;
+vector<vc> g;
+vc path;
+vector<vb> isVisited;
+coor start, destination;
+
+void printPath() {
+    cout << endl << "YES" << endl;
+    cout << path.size() << endl;
+    for(auto c: path) {
+        cout << c;
+    }
+    cout << endl;
+}
+
+void findPath(bfs p) {
+    queue<bfs > q;    
+    q.push(p);
+    while(!q.empty()) {
+        coor t = p.first; 
+        for(int i=0; i<4; i++) {
+            if(g[t.y+y[i]][t.x+x[i]]!='#') {
+                coor next = {t.x+x[i], t.y+y[i]}
+                q.push(make_pair(next,p.second);
+            }
+        }        
     }
 }
 
@@ -31,32 +54,25 @@ int main() {
         isVisited[i].resize(n+2);
     }
 
-    for(int i=0; i<n+2; n++) {
-        for(int j=0; j<n+2; j++) {
+    for(int i=0; i<n+2; i++) {
+        for(int j=0; j<m+2; j++) {
             isVisited[i][j]=true;
-            if(i>0&&j>0&&i<n+1&&j<n+1) {
+            if(i>0&&j>0&&i<n+1&&j<m+1) {
                 cin >> g[i][j];
                 if(g[i][j]=='A') {
                     start.y=i;
                     start.x=j;
                 }
                 if(g[i][j]=='B') {
-                    end.y=i;
-                    end.x=j;
+                    destination.y=i;
+                    destination.x=j;
                 }
                 isVisited[i][j]=false;
             }
         }
     }
 
-    findPath(start.y, start.x);
-    
-    cout << path.size() << endl;
-    for(auto c: path) {
-        cout << c;
-    }
+    findPath(make_pair(start,' '));
 
-    cout << endl;
-    
     return 0;
 }
