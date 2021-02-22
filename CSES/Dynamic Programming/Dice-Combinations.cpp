d1fa798cf7bb3428c8da6bff17b95c3e15c94052
Trieu Truong <khactrieu.personal@gmail.com>
2021-02-23 01:51:12 +0700

    add problem

--- CSES/Dynamic Programming/Dice-Combinations.cpp
+++ CSES/Dynamic Programming/Dice-Combinations.cpp
@@ -2,21 +2,47 @@
 using namespace std;
 
 #define MAXN 1000001
-#define MOD 1000000007
+
+const int MOD = 1000000007;
 
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int,int> pii;
 
-int n;
-vector<vi> dp;
+int n, ans;
+vector<int> subtract(7,0);
 
 int main() {
     cin >> n;
-    dp.resize(n+1);
-    for(int i=0; i<=n; i++) {
-        dp[i].resize(n+1);
+
+    subtract[0]=1;
+    for(int i=1; i<=6; i++) {
+        for(int j=1; j<=6; j++) {
+            if(i>=j) subtract[i]+=subtract[i-j] % MOD;
+        }
     }
+
+    if(n<=6) {
+        cout << subtract[n] << endl;
+        return 0;
+    }
+
+    for(int i=7; i<=n; i++) {
+        ans=0;
+        for(int j=1; j<=6; j++) {
+            ans += subtract[j] % MOD;
+            ans %= MOD;
+        }
+
+        for(int j=1; j<6; j++) {
+            subtract[j]=subtract[j+1]%MOD;
+        }
+
+        subtract[6]=ans;
+    }
+
+    cout << ans << endl;
+
     return 0;
 }
