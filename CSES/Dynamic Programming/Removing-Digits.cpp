ecb2597c9f75935084758c76e3b3f8c40c01508a
Trieu Truong <khactrieu.personal@gmail.com>
2021-02-25 23:37:29 +0700

    update problem

--- CSES/Dynamic Programming/Removing-Digits.cpp
+++ CSES/Dynamic Programming/Removing-Digits.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX = 1e6;
 int n;
 vector<int> dp;
 
 int main() {
     cin >> n;
-    dp.resize(n+1, INT_MAX);
+    dp.resize(n+1, MAX);
     dp[0]=0;
 
     for(int value=1; value<=n; value++) {
         int v=value;
         while(v>0) {
             int digit=v%10;
-            dp[v]=min(dp[v],dp[v-digit]);
-            v /= 10;
+            dp[value]=min(dp[value],dp[value-digit]+1);
+            v/=10;
         }
-
-        dp[v]++;
     }
 
     cout << dp[n] << endl;
